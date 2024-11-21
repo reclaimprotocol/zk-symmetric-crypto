75bc4bd8d489308a3a79b428853990ae2567b14e
Alex E <alex@creatoros.co>
2024-11-21 09:42:08 +0500

    [CHORE] refactored AES

--- gnark/circuits/aesV2_oprf/aes128_test.go
+++ gnark/circuits/aesV2_oprf/aes128_test.go
@@ -1,19 +1,3 @@
-/*
-Copyright © 2023 Jan Lauinger
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package aes_v2_oprf
 
 import (
@@ -38,12 +22,12 @@ func TestAES128(t *testing.T) {
 	key := "7E24067817FAE0D743D6CE1F32539163"
 	Nonce := "006CB6DBC0543B59DA48D90B"
 
-	secretStr := "00000000001111111111000000000011" // max 62 bytes
+	secretStr := "00000000001111111111000000000011000000000011111111110000000000" // max 62 bytes
 	secretBytes := []byte(secretStr)
 	d, err := toprf.PrepareTestData(secretStr, "reclaim")
 	assert.NoError(err)
 
-	pos := 1
+	pos := 18
 	Counter := 12345
 	plaintext := make([]byte, BLOCKS*16)
 	copy(plaintext[pos:], secretBytes)
--- gnark/circuits/aesV2_oprf/common.go
+++ gnark/circuits/aesV2_oprf/common.go
@@ -10,7 +10,8 @@ import (
 	"github.com/consensys/gnark/std/math/cmp"
 )
 
-const BLOCKS = 4
+const BLOCKS = 5
+const NB = 4
 const BytesPerElement = 31
 
 type TOPRFData struct {
@@ -47,10 +48,11 @@ type AESGadget struct {
 	sbox           *logderivlookup.Table
 	RCon           [11]frontend.Variable
 	t0, t1, t2, t3 *logderivlookup.Table
+	keySize        int
 }
 
 // retuns AESGadget instance which can be used inside a circuit
-func NewAESGadget(api frontend.API) AESGadget {
+func NewAESGadget(api frontend.API, keySize int) AESGadget {
 
 	t0 := logderivlookup.New(api)
 	t1 := logderivlookup.New(api)
@@ -67,7 +69,31 @@ func NewAESGadget(api frontend.API) AESGadget {
 
 	RCon := [11]frontend.Variable{0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36}
 
-	return AESGadget{api: api, sbox: sbox, RCon: RCon, t0: t0, t1: t1, t2: t2, t3: t3}
+	return AESGadget{api: api, sbox: sbox, RCon: RCon, t0: t0, t1: t1, t2: t2, t3: t3, keySize: keySize}
+}
+
+func (aes *AESWrapper) Define(api frontend.API, keySize int) error {
+	counter := aes.Counter
+	var counterBlock [16]frontend.Variable
+
+	gAes := NewAESGadget(api, keySize)
+
+	for i := 0; i < 12; i++ {
+		counterBlock[i] = aes.Nonce[i]
+	}
+	for b := 0; b < BLOCKS; b++ {
+		gAes.createIV(counter, counterBlock[:])
+		// encrypt counter under key
+		keystream := gAes.Encrypt(aes.Key, counterBlock)
+
+		for i := 0; i < 16; i++ {
+			api.AssertIsEqual(aes.Out[b*16+i], gAes.VariableXor(keystream[i], aes.In[b*16+i], 8))
+		}
+		counter = api.Add(counter, 1)
+	}
+	api.AssertIsEqual(counter, api.Add(aes.Counter, BLOCKS))
+
+	return aes.TOPRFVerify(api)
 }
 
 // aes128 encrypt function
@@ -139,7 +165,106 @@ func (aes *AESGadget) createIV(counter frontend.Variable, iv []frontend.Variable
 	for i := 0; i < 4; i++ {
 		iv[15-i] = aes.api.FromBinary(aBits[i*8 : i*8+8]...)
 	}
+}
 
+func (aes *AESGadget) Encrypt(key []frontend.Variable, pt [16]frontend.Variable) [16]frontend.Variable {
+	keySize := aes.keySize
+	rounds := 10
+	if keySize == 32 {
+		rounds = 14
+	}
+
+	// expand key
+	xk := aes.ExpandKey(key)
+	var state [16]frontend.Variable
+	for i := 0; i < 16; i++ {
+		state[i] = aes.VariableXor(xk[i], pt[i], 8)
+	}
+
+	var t0, t1, t2, t3 []frontend.Variable
+	// iterate rounds
+	for i := 1; i < rounds; i++ {
+		k := i * 16
+		t0 = aes.XorSubWords(state[0], state[5], state[10], state[15], xk[k+0:k+4])
+		t1 = aes.XorSubWords(state[4], state[9], state[14], state[3], xk[k+4:k+8])
+		t2 = aes.XorSubWords(state[8], state[13], state[2], state[7], xk[k+8:k+12])
+		t3 = aes.XorSubWords(state[12], state[1], state[6], state[11], xk[k+12:k+16])
+
+		copy(state[:4], t0)
+		copy(state[4:8], t1)
+		copy(state[8:12], t2)
+		copy(state[12:16], t3)
+	}
+
+	copy(state[:], aes.ShiftSub(state))
+
+	k := rounds * 16
+
+	for i := 0; i < 4; i++ {
+		state[i+0] = aes.VariableXor(state[i+0], xk[k+i+0], 8)
+		state[i+4] = aes.VariableXor(state[i+4], xk[k+i+4], 8)
+		state[i+8] = aes.VariableXor(state[i+8], xk[k+i+8], 8)
+		state[i+12] = aes.VariableXor(state[i+12], xk[k+i+12], 8)
+	}
+
+	return state
+}
+
+func (aes *AESGadget) ExpandKey(key []frontend.Variable) []frontend.Variable {
+
+	keySize := aes.keySize
+	rounds := 10
+	if keySize == 32 {
+		rounds = 14
+	}
+
+	var nWords = NB * (rounds + 1)
+
+	expand := make([]frontend.Variable, nWords*4)
+	i := 0
+
+	for i < keySize {
+		expand[i] = key[i]
+		expand[i+1] = key[i+1]
+		expand[i+2] = key[i+2]
+		expand[i+3] = key[i+3]
+
+		i += 4
+	}
+
+	for i < (nWords * 4) {
+		t0 := expand[i-4]
+		t1 := expand[i-3]
+		t2 := expand[i-2]
+		t3 := expand[i-1]
+
+		if i%keySize == 0 {
+			// rotation
+			t0, t1, t2, t3 = t1, t2, t3, t0
+
+			// sub words
+			tt := aes.Subws(aes.sbox, t0, t1, t2, t3)
+			t0, t1, t2, t3 = tt[0], tt[1], tt[2], tt[3]
+
+			t0 = aes.VariableXor(t0, aes.RCon[i/keySize], 8)
+		}
+
+		if rounds == 14 && i%keySize == 16 {
+			// subwords
+			tt := aes.Subws(aes.sbox, t0, t1, t2, t3)
+			t0, t1, t2, t3 = tt[0], tt[1], tt[2], tt[3]
+
+		}
+
+		expand[i] = aes.VariableXor(expand[i-keySize], t0, 8)
+		expand[i+1] = aes.VariableXor(expand[i-keySize+1], t1, 8)
+		expand[i+2] = aes.VariableXor(expand[i-keySize+2], t2, 8)
+		expand[i+3] = aes.VariableXor(expand[i-keySize+3], t3, 8)
+
+		i += 4
+	}
+
+	return expand
 }
 
 func (circuit *AESWrapper) TOPRFVerify(api frontend.API) error {

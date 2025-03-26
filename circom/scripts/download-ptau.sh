0f3e2778e2d4686d80bad6475593e043eac2fc2b
Gauravbhatia1211 <68150602+Gauravbhatia1211@users.noreply.github.com>
2025-03-26 23:40:02 +0530

    Update download-ptau.sh

--- circom/scripts/download-ptau.sh
+++ circom/scripts/download-ptau.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-# Official trusted checksum for powersOfTau28_hez_final_17.ptau
-OFFICIAL_CHECKSUM="6247a3433948b35fbfae414fa5a9355bfb45f56efa7ab4929e669264a0258976741dfbe3288bfb49828e5df02c2e633df38d2245e30162ae7e3bcca5b8b49345"  
+# Official trusted BLAKE2b-512 checksum for powersOfTau28_hez_final_17.ptau
+OFFICIAL_CHECKSUM="6247a3433948b35fbfae414fa5a9355bfb45f56efa7ab4929e669264a0258976741dfbe3288bfb49828e5df02c2e633df38d2245e30162ae7e3bcca5b8b49345"
 
+# Create pot directory if it doesn't exist
 echo "Downloading official ptau file..."
 curl -o pot/pot_final.ptau https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_17.ptau
 
-echo -e "\nVerifying checksum..."
-DOWNLOADED_CHECKSUM=$(sha256sum pot/pot_final.ptau | awk '{print $1}')
+echo -e "\nVerifying BLAKE2b-512 checksum..."
+DOWNLOADED_CHECKSUM=$(b2sum -l 512 pot/pot_final.ptau | awk '{print $1}')
 
 if [ "$DOWNLOADED_CHECKSUM" != "$OFFICIAL_CHECKSUM" ]; then
     echo -e "\n\033[31mDANGER: Checksum verification failed!\033[0m"

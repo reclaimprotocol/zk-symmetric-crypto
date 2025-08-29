9cb9a18db41882f2566b4c15b44e1754433f693c
Reclaim CI <hi@reclaimprotocol.org>
2025-08-29 04:38:31 +0000

    chore: publish v5.0.0

--- js/src/config.ts
+++ js/src/config.ts
@@ -3,7 +3,7 @@ import type { AlgorithmConfig, EncryptionAlgorithm } from './types.ts'
 import { bitsToUint8Array, bitsToUintArray, toUint8Array, toUintArray, uint8ArrayToBits, uintArrayToBits } from './utils.ts'
 
 // commit hash for this repo
-export const GIT_COMMIT_HASH = '2fcb282deb2b994a3ea4dd9039630ce2f94df8bf'
+export const GIT_COMMIT_HASH = '3665e6c738b9b6429c020027155b665816d4a703'
 
 export const CONFIG: { [E in EncryptionAlgorithm]: AlgorithmConfig } = {
 	'chacha20': {
